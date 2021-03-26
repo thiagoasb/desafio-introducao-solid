402f5eb0ce834269ff4f977d39e6532b111354e0
thiagoasb <thiagoasbezerra@gmail.com>
2021-03-25 23:21:13 -0300

    challange complete

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,16 +2,24 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        const emailAlreadyUsed = this.usersRepository.findByEmail(email);
+
+        if (emailAlreadyUsed) {
+            throw new Error("Email already used!");
+        }
+
+        const user = this.usersRepository.create({ name, email });
+
+        return user;
+    }
 }
 
 export { CreateUserUseCase };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,26 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const users = this.usersRepository.list();
+        const user = this.usersRepository.findById(user_id);
+
+        if (!user) {
+            throw new Error("User not exists!");
+        }
+
+        if (!user.admin) {
+            throw new Error("User is not admin!");
+        }
+
+        return users;
+    }
 }
 
 export { ListAllUsersUseCase };
